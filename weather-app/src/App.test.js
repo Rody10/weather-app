8218616bb80c634b79bf118e23ff77454df434f0
Rody10 <rodneysimbatholanah@gmail.com>
2023-08-23 22:23:07 +0200

    added test for CurrentWeather component

--- weather-app/src/App.test.js
+++ weather-app/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import CurrentWeather from './components/current-weather';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+describe('CurrentWeather Component', () => {
+  const testData = {
+    cityName: 'City',
+    countryName: 'Country',
+    currentWeather: {
+      temperature: 20,
+      weathercode: 800,
+      windspeed: 10,
+      winddirection: 45,
+    },
+  };
+
+  it('renders weather data correctly', () => {
+    const { getByText } = render(<CurrentWeather data={testData} />);
+
+    expect(getByText('City, Country')).toBeInTheDocument();
+    expect(getByText('20.0°C')).toBeInTheDocument();
+    expect(getByText('Weather condition:')).toBeInTheDocument();
+    expect(getByText('Wind speed:')).toBeInTheDocument();
+    expect(getByText('Wind direction:')).toBeInTheDocument();
+  });
 });
+
